66e4cb8fd806a5631e3f1f4d9bec5cae0a87f5cd
Lio <leo036300@gmail.com>
2019-09-17 01:01:25 +0200

    Using vscode/typescript for intellisense

---
+++ docs/typechecking.md
@@ -0,0 +1,19 @@
+Using typescript for javascript intellisense.
+===
+
+WIP, slowly growing.
+
+Usage: 
+---
+
+Testet with vscode.
+
+* Put your cards folder as subfolder here.
+* reference gamebuilder.d.ts (in root folder) by
+* * Having gamebuilder.d.ts open,
+* * or some file with this comment at top:
+* * `/// <reference path="../gamebuilder.d.ts" />`
+* * * adjust relative path
+* * * can be in the cardfile itself
+* run in root to get THREE.Vector3 etc: `npm install` 
+* Use intellisense.
---
+++ gamebuilder.d.ts
@@ -0,0 +1,49 @@
+// see docs/typechecking.md
+
+import * as THREE from "three";
+
+declare global {
+
+    interface propsAreInbuild { [key: string]: any }
+    const props: propsAreInbuild;
+
+    type ActorRef = { comment: "actually a string", docHack: "else vscode hints 'string'" };
+
+    // functions
+
+    function clone(original, pos?, rot?): ActorRef;
+    function deltaTime(): number;
+    function detachFromParentPlease(actor: ActorRef): void;
+    function detachFromParentPlease(actor: ActorRef): void;
+    function getActorGroupDescription(pattern: string): string;
+    function getActors(): ActorRef[];
+    function getActorsInGroup(pattern: string): ActorRef[];
+    function getActorsWithTag(tag: string): ActorRef[];
+    function getBoundsRadiusOuter(actor: ActorRef, includeVertical?: boolean): number;
+    function getBoundsRadiusOuter(includeVertical?: boolean): number;
+    function getCardTargetActor(propName: string, actionMessage: any): ActorRef | null;
+    function getDisplayName(actor: ActorRef): string;
+    function getParent(actor?: ActorRef): ActorRef | null;
+    function getPointBehind(dist: number, actor?: ActorRef): THREE.Vector3;
+    function getPos(actor?: ActorRef): THREE.Vector3;
+    function getThrottle(actor: ActorRef): THREE.Vector3;
+    function getTimeSinceReset();
+    function getVar(name: string, actor: ActorRef): any;
+    function getVelocity(actor: ActorRef): THREE.Vector3;
+    function isClone(actor?): boolean;
+    function log(msg: string): void;
+    function lookAt(targetActorOrPoint: ActorRef | THREE.Vector3, yawOnly?: boolean, maxRadians?: number): void;
+    function myself(): ActorRef;
+    function overlapSphere(center: THREE.Vector3, radius: number): ActorRef;
+    function propActor(prop: string): ActorRef;
+    function propActorGroup(prop: string);
+    function propEnum(name: string, defaultValue: string, allowedValues: string[], options?);
+    function propNumber(prop: string, val: number);
+    function send(actor: ActorRef, messageName: string, arg?: any): void;
+    function setKinematic(isKinematic: boolean);
+    function setPos(worldPos: THREE.Vector3): void;
+    function setTintHex(colorHex: string): void;
+    function setVar(name: string, value: any): void;
+    function turn(radians: number, axis?: THREE.Vector3): void;
+    function vec3(x: number, y: number, z: number): THREE.Vector3;
+}
